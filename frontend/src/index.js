eab21a6f669ced620867b9a68f71cbce9138f908
LamCam <lamcam813@gmail.com>
2024-03-28 09:50:12 +0700

    Set layout product page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Test from './partials/Test'; // file test component
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      {/* <Test/> */}
-     {/* <App></App> */}
-     <Home></Home>
+     <App></App>
+     {/* <Home></Home> */}
   </React.StrictMode>
      
 );
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,15 +1,27 @@
+import { Button } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 
 export default function Products() {
     return (
-        <Container fluid>
-            <Row>filter san pham ne</Row>
+        <Container>
             <Row>
-                <Col>danh muc</Col>
-                <Col>san pham</Col>
+                <Col><p>Sắp xếp theo: </p></Col>
+                <p className="product__title label-large on-surface-text">Sắp xếp theo</p>
+                <Button className={`btn-icon-label product__button js-button btn--outlined discount `}>
+                    <span className="label">Giảm giá</span>
+                </Button>
+
+                <Button className={`product__button js-button btn--outlined best-seller `}>
+                    Bán chạy nhất
+                </Button>
+
+            </Row>
+            <Row>
+                <Col lg={3}>danh muc</Col>
+                <Col lg={9}>san pham</Col>
             </Row>
         </Container>
     );
