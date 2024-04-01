49a4263672dfb4e49252d223d3c2d64240e0a226
daubuidaubui <yengiau123@gmail.com>
2024-04-01 13:06:37 +0700

    Create aboutUs

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <div>
-            <h1>About Us</h1>
-            <p>Welcome to our website! We are a team of passionate individuals dedicated to providing high-quality products and services.</p>
-            <p>Our mission is to make a positive impact in the world through innovative solutions and exceptional customer experiences.</p>
-            <p>Feel free to explore our website and learn more about what we do.</p>
+           <div class="about__content container">
+        <section class="mt-48">
+          <h1 class="section__title display-large">VỀ CHÚNG TÔI</h1>
+          <div class="intro row mt-12">
+            <p class="information col-9 col-sm-12 body-large mt-12">TAA là thương
+              hiệu phụ kiện dẫn dầu xu hướng với phong cách thiết kế đơn giản,
+              sành điệu, sản phẩm tiêu chuẩn cao dành cho giới trẻ toàn cầu tự
+              tin thể hiện cá tính và phong cách riêng. Được thành lập vào năm
+              2023, chúng tôi đã trở thành nhà phân phối đầu tiên đi tiên phong
+              với phân khúc sản phẩm siêu hời dưới "99.000vnđ" nhằm mang lại góc
+              nhìn khác hơn về giá thành của thương hiệu Việt.</p>
+          </div>
+        </section>
+      </div>
         </div>
     );
 };
