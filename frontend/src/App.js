23fbf0a6937a5536aa6f113c506e48896bd29d94
LamCam <lamcam813@gmail.com>
2024-04-02 23:40:56 +0700

    Update pages/Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import RootLayout from "./routes/RootLayout";
-import Home from "./pages/Home/Home";
-import News from "./pages/News/News";
-import Policy from "./pages/Policy/Policy";
-import AboutUs from "./pages/AboutUs/AboutUs";
-import Products from "./pages/Products/Products";
-import Error404 from "./pages/Error404";
+import RootLayout from './routes/RootLayout';
+import Home from './pages/Home/Home';
+import News from './pages/News/News';
+import Policy from './pages/Policy/Policy';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Products from './pages/Products/Products';
+import Error404 from './pages/Error404';
 import {
   BrowserRouter,
   Route,
@@ -12,22 +12,23 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" 
-           element={<RootLayout></RootLayout>} 
-           errorElement={<Error404 />}>
-
-        <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
-        <Route path="news" element={<News />} />
-        <Route path="about_us" element={<AboutUs />} />
-        <Route path='guideline' element={<></>} >  </Route>
-        <Route path='policy' element={<Policy/>} >  </Route>
-    </Route>
-  )
+    <Route path="/" element={<RootLayout></RootLayout>} errorElement={<Error404 />}>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="news" element={<News />} />
+      <Route path="about_us" element={<AboutUs />} />
+      <Route path="guideline" element={<></>}>
+        {' '}
+      </Route>
+      <Route path="policy" element={<Policy />}>
+        {' '}
+      </Route>
+    </Route>,
+  ),
 );
 
 function App() {
