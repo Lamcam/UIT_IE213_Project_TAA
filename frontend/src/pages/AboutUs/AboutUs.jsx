425202e133c3985a45d223282d95209f504357fd
daubuidaubui <yengiau123@gmail.com>
2024-04-02 23:56:28 +0700

    Update pages/AboutUs

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import "./AboutUs.scss";
+import { Container } from 'react-bootstrap';
 
-const AboutUs = () => {
+function AboutUs() {
     return (
-        <div>
-           <div class="about__content container">
+      <Container>
+      <div class="about__content container">
         <section class="mt-48">
           <h1 class="section__title display-large">VỀ CHÚNG TÔI</h1>
           <div class="intro row mt-12">
+            <img class="information col-3 col-sm-12" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/433001636_764025502495269_5962452863049639980_n.png?stp=dst-png_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHCRjzxlwLOCHnD8DnRLqYUvfQQujb4Hga99BC6NvgeBhq9TXM7BvOJcNx-Ey-5ek9gCkaVAYKTQEYuNhaNVSTT&_nc_ohc=JyjMcp1FYDwAX8egRki&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfARj_h7WpcdyJotj23gWx0FCYr2ZAeUPyxRVCMbD6J0mA&oe=661161A9" alt="Logo TAA"/>
             <p class="information col-9 col-sm-12 body-large mt-12">TAA là thương
               hiệu phụ kiện dẫn dầu xu hướng với phong cách thiết kế đơn giản,
               sành điệu, sản phẩm tiêu chuẩn cao dành cho giới trẻ toàn cầu tự
@@ -17,7 +20,57 @@ const AboutUs = () => {
           </div>
         </section>
       </div>
-        </div>
+
+      <div class="mt-48">
+        <img class="picture1" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/433001636_764025502495269_5962452863049639980_n.png?stp=dst-png_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHCRjzxlwLOCHnD8DnRLqYUvfQQujb4Hga99BC6NvgeBhq9TXM7BvOJcNx-Ey-5ek9gCkaVAYKTQEYuNhaNVSTT&_nc_ohc=JyjMcp1FYDwAX8egRki&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfARj_h7WpcdyJotj23gWx0FCYr2ZAeUPyxRVCMbD6J0mA&oe=661161A9" alt="Về chúng tôi"/>
+      </div>
+
+      <div class="about__content container">
+        <section class="mt-64">
+          <div class="content__title row">
+            <div class="col-6 col-sm-12">
+              <ul>
+                <li class="display-small">WHAT WE DO</li>
+                <li class="display-small">Young generatio ally</li>
+              </ul>
+            </div>
+            <p class="col-6 col-sm-12 information__content body-large">Sứ mệnh
+              của TAA là Cổ vũ cho thế hệ trẻ toàn thế giới tự do thể hiện phong
+              cách thông qua thời trang, thương hiệu vượt qua ranh giới của thời
+              trang đường phố bằng cách không ngừng sáng tạo các trang phục trong
+              các bộ sưu tập độc đáo. TAA mong muốn đồng hành và tôn vinh thế hệ
+              trẻ tài năng qua những xu hướng thời trang và hoạt động cộng đồng
+              sáng tạo. Từ đó tạo dấu ấn cho giá trị bền vững để cùng các tài năng
+              trẻ phát triển.</p>
+          </div>
+        </section>
+      </div>
+
+      <div class="mt-48">
+        <img class="picture2" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/433001636_764025502495269_5962452863049639980_n.png?stp=dst-png_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHCRjzxlwLOCHnD8DnRLqYUvfQQujb4Hga99BC6NvgeBhq9TXM7BvOJcNx-Ey-5ek9gCkaVAYKTQEYuNhaNVSTT&_nc_ohc=JyjMcp1FYDwAX8egRki&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfARj_h7WpcdyJotj23gWx0FCYr2ZAeUPyxRVCMbD6J0mA&oe=661161A9" alt="Cửa hàng"/>
+      </div>
+
+      <div class="about__content">
+        <section class="mt-48">
+          <div class="contact__info container row">
+            <div class="contact col-6 col-sm-12">
+              <p class="section__title display-large">LIÊN HỆ</p>
+            </div>
+            <div class="contact col-6 col-sm-12">
+              <ul class="contact__list">
+                <li class="body-large">Instagram: <a href="https://daa.uit.edu.vn/"> TAA_Shop</a></li>
+                <li class="body-large">Shopee: <a href="https://daa.uit.edu.vn/"> TAA_Shop - Three Accessories Appreciate</a></li>
+                <li class="body-large">Facebook: <a href="https://daa.uit.edu.vn/"> TAA</a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div class="mt-48">
+        <img class="picture3" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/433001636_764025502495269_5962452863049639980_n.png?stp=dst-png_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHCRjzxlwLOCHnD8DnRLqYUvfQQujb4Hga99BC6NvgeBhq9TXM7BvOJcNx-Ey-5ek9gCkaVAYKTQEYuNhaNVSTT&_nc_ohc=JyjMcp1FYDwAX8egRki&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfARj_h7WpcdyJotj23gWx0FCYr2ZAeUPyxRVCMbD6J0mA&oe=661161A9" alt="Định hướng"/>
+      </div>
+      </Container>
     );
 };
 
