552f2beec64a6b0284876be52e82c27db922ccda
anhuynh219 <anhuynh200319@gmail.com>
2024-04-15 19:23:56 +0700

    Update privacy for Login Register and schema of user data(update id)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,14 @@
 const User = require("../models/users.model");
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const SerectKey = 'HAN';
+
+const createToken =(_id) => {
+   return jwt.sign({ _id }, SerectKey, { expiresIn: '1h' });
+}
 
 const getUser = async (req, res) => {
     try {
@@ -7,8 +16,6 @@ const getUser = async (req, res) => {
         console.log(user);
 
         res.cookie('user', true, { maxAge: 900000, httpOnly: true });
-        console.log('Cookies:', req.cookies.user);
-
         res.status(200).json(user);
 
     } catch (error) {
@@ -19,15 +26,24 @@ const getUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
-
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please fill in all fields" });
+        }
+        
         const user = await User.findOne({user_email : email});
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
-        if (user.user_pass !== password) {
+        
+        const match = await bcrypt.compare(password, user._id);
+
+        if (!match) {
             return res.status(400).json({ message: "Invalid password" });
         }
-        res.status(200).json(user);
+
+        const token = createToken(user._id);
+        res.status(200)
+        .json([user,token])
         console.log('Login success');
     }
     catch (error) {
@@ -39,23 +55,29 @@ const registerUser = async (req, res) => {
     try {
         const { name, phone, email, password } = req.body;
         const user = await User.findOne({ user_email: email });
+        
         if (user) {
             return res.status(400).json({ message: "User already exists" });
         }
         else{
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            
             const newUser = new User({
+                _id: new mongoose.Types.ObjectId(),
                 user_name: name,
                 user_phone: phone,
                 user_email: email,
-                user_pass: password,
+                user_pass: hashedPassword,
                 user_avatar: '',
                 local_default_id: '',
                 bank_default_id: '',
             });
             await newUser.save();
-            res.status(201).json(newUser);
+            console.log(newUser);
+            const token = await createToken(newUser._id);
+            res.status(201).json([newUser,token]);
         }
-        
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -78,10 +100,21 @@ const updatePassword = async(req, res) => {
 
 }
 
-const auth ={
+const logOut = async (req, res) => {
+    try {
+        res.clearCookie('user');
+        res.clearCookie('auth');
+        res.status(200).json({ message: "Logout success" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const auth = {
     getUser,
     registerUser,
     loginUser,
     updatePassword,
+    logOut,
 }
 module.exports = auth;
--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let UsersSchema = new Schema({
+    _id: { type: "string", required: true },
     user_name: { type: "string", required: true },
-    user_phone: { type: "string", required: true },
-    user_email: { type: "string", required: true },
+    user_phone: { type: "string", required: true,},
+    user_email: { type: "string" , required: true, uniquer: true},
     user_pass: { type: "string", require: true },
     user_avatar: { type: "string" }, //chỉnh lại sau (HAN chỉnh từ null thành string)
     local_default_id: { type: "string" }, // HAN delete required: true
