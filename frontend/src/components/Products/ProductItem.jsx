75200a86dbf53a2171c37a7f3dde55344e0104ad
LamCam <lamcam813@gmail.com>
2024-04-18 00:11:33 +0700

    Fix bug filter product

--- frontend/src/components/Products/ProductItem.jsx
+++ frontend/src/components/Products/ProductItem.jsx
@@ -43,6 +43,10 @@ function ProductItem({ product }) {
         return formattedPrice.trim();
     };
 
+    if (!product) {
+        return null;
+      }
+
     const currentPrice = formatPrice(product.prod_cost.$numberDecimal - product.prod_discount.$numberDecimal * product.prod_cost.$numberDecimal)
     const discount = product.prod_discount.$numberDecimal * 100
     const BeforDiscountPrice = formatPrice(product.prod_cost.$numberDecimal)
@@ -59,7 +63,8 @@ function ProductItem({ product }) {
                 ) : (
                     <TbHeartPlus className='icon-heart' onClick={toggleLike} />
                 )}
-                <div className="product__item__name on-error-container-text">{product.prod_name}</div>
+                {/* <div className="product__item__name on-error-container-text">{product.prod_name}</div> */}
+                <NavLink to={`/products/${product._id}`} className="product__item__name on-error-container-text">{product.prod_name}</NavLink>
                 <div className="product__item__price">
                     <div className="item__price__current">{currentPrice} đ</div>
                     <div className="item__price__discount">{BeforDiscountPrice} đ</div>
--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -12,7 +12,7 @@ import ProductFilter from 'components/Products/ProductFilter';
 function Products() {
 
   const [data, setData] = useState([]);
-
+  const [filteredData, setFilteredData] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
@@ -21,17 +21,97 @@ function Products() {
     try {
       const response = await axios.get('http://localhost:8000/products');
       setData(response.data);
-      console.log(response.data)
+      setFilteredData(response.data); // Khởi tạo filteredData ban đầu là data từ API
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  const applyFilter = (filterOption) => {
-    // Áp dụng bộ lọc vào dữ liệu và cập nhật state data
-    // Ví dụ: lọc sản phẩm theo filterOption và cập nhật state data
-    // setData(filteredData);
+
+  const applyFilter = ({ sortBy, filterBy }) => {
+    let updatedData = [...data];
+
+    // Xử lý sắp xếp theo giá
+    if (sortBy === '1') {
+      if (filterBy && filterBy.includes('Bán chạy nhất')) { updatedData.sort((a, b) => b.prod_num_sold - a.prod_num_sold); }
+      if (filterBy && filterBy.includes('Giảm giá')) {
+        updatedData = updatedData.filter(item => parseFloat(item.prod_discount.$numberDecimal) > 0);
+        console.log("trong if truoc sort1")
+        console.log(updatedData)
+
+      }
+      console.log("ngoai if truoc sort1")
+      console.log(updatedData)
+      updatedData.sort((a, b) => {
+        const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+        const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+        return priceA - priceB;
+      });
+      console.log("sau sort1")
+      console.log(updatedData)
+    } else if (sortBy === '2') {
+      if (filterBy && filterBy.includes('Bán chạy nhất')) { updatedData.sort((a, b) => b.prod_num_sold - a.prod_num_sold); }
+      if (filterBy && filterBy.includes('Giảm giá')) {
+        updatedData = updatedData.filter(item => parseFloat(item.prod_discount.$numberDecimal) > 0);
+        console.log("trong if truoc sort2")
+        console.log(updatedData)
+      }
+
+      console.log("ngoai if truoc sort2")
+      console.log(updatedData)
+      updatedData.sort((a, b) => {
+        const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+        const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+        return priceB - priceA;
+      });
+      console.log("sau sort12")
+      console.log(updatedData)
+    }
+
+    // Xử lý bộ lọc theo sản phẩm bán chạy nhất
+    if (filterBy && filterBy.includes('Bán chạy nhất')) {
+      updatedData.sort((a, b) => b.prod_num_sold - a.prod_num_sold);
+      if (sortBy === '1') {
+        updatedData.sort((a, b) => {
+          const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+          const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+          return priceA - priceB;
+        });
+      } else if (sortBy === '2') {
+        updatedData.sort((a, b) => {
+          const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+          const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+          return priceB - priceA;
+        });
+      }
+
+      console.log(updatedData)
+    }
+
+    // Xử lý bộ lọc theo giảm giá
+    if (filterBy && filterBy.includes('Giảm giá')) {
+      updatedData = updatedData.filter(item => parseFloat(item.prod_discount.$numberDecimal) > 0);
+      console.log("hellsso")
+      if (sortBy === '1') {
+        updatedData.sort((a, b) => {
+          const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+          const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+          return priceA - priceB;
+        });
+      } else if (sortBy === '2') {
+        updatedData.sort((a, b) => {
+          const priceA = parseFloat(a.prod_cost.$numberDecimal) - parseFloat(a.prod_discount.$numberDecimal) * parseFloat(a.prod_cost.$numberDecimal);
+          const priceB = parseFloat(b.prod_cost.$numberDecimal) - parseFloat(b.prod_discount.$numberDecimal) * parseFloat(b.prod_cost.$numberDecimal);
+          return priceB - priceA;
+        });
+      }
+    }
+
+    setFilteredData(updatedData);
+
   };
+
+
   return (
     <Container className="product" fluid>
       <ProductFilter applyFilter={applyFilter} />
@@ -41,7 +121,7 @@ function Products() {
         </Col>
         <Col lg={9} md={9} className="product__list">
           <Row className="row-cols-1 row-cols-md-3 g-3">
-            {data.map((product) => (
+            {filteredData.map((product) => (
               <Col key={product._id} lg={4} md={6} xs={6}>
                 <ProductItem product={product} />
               </Col>
