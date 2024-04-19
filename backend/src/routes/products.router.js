8261037efd4428af93abf6e25f15f1f6a04fee9a
LamCam <lamcam813@gmail.com>
2024-04-19 19:57:13 +0700

    Update router product

--- backend/src/routes/products.router.js
+++ backend/src/routes/products.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const product = require("../controllers/products.controller");
 
-router.get("/:cate_type_name/:cate_name", product.getProductsByCategory);
-router.get("/:cate_type_name", product.getProductsByCategoryType);
 router.get("/", product.getProducts);
+router.get("/category/:cate_type_name", product.getProductsByCategoryType);
+router.get("/category/:cate_type_name/:cate_name", product.getProductsByCategory);
 router.get("/:id", product.getProductById);
 
 module.exports = router;
