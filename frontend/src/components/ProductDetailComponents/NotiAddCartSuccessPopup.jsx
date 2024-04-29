122b4c5272f4822d2f3b9d869210164ceeedfe54
LamCam <lamcam813@gmail.com>
2024-04-30 04:12:15 +0700

    Fix popup noti cart

--- frontend/src/components/ProductDetailComponents/NotiAddCartSuccessPopup.jsx
+++ frontend/src/components/ProductDetailComponents/NotiAddCartSuccessPopup.jsx
@@ -5,7 +5,7 @@ import { GoCheckCircleFill } from 'react-icons/go';
 
 function NotiAddCartSuccessPopup(props) {
   return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal {...props} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton className="modal-noti-add-cart-success">
         {/* <Modal.Title id="contained-modal-title-vcenter">Modal heading</Modal.Title> */}
       </Modal.Header>
