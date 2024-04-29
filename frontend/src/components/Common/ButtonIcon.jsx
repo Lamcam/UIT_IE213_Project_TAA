046dc754e4d30b505f4049c0f667180f25cdee7e
LamCam <lamcam813@gmail.com>
2024-04-29 16:21:56 +0700

    Add labe color in ButtonIcon

--- frontend/src/components/Common/ButtonIcon.jsx
+++ frontend/src/components/Common/ButtonIcon.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const ButtonIcon = ({ className, label, type, onClick, height, width, backgroundColor, marginRight, border }) => {
+const ButtonIcon = ({ className, label, type, onClick, height, width, backgroundColor, marginRight, border, labelColor }) => {
     const buttonIconStyle = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        height: height||'40px',
-        width: width||'40px',
+        height: height || '40px',
+        width: width || '40px',
         borderRadius: '0.75rem',
-        border: border|| '1px solid #785B5B',
+        border: border || '1px solid #785B5B',
         padding: 0,
         backgroundColor: backgroundColor,
         marginRight: marginRight,
+        color: labelColor || 'black',
     }
     return (
         <button className={className} type={type} style={buttonIconStyle} onClick={onClick}>
