ceb6fccc752ab58f4ffa63557134040213b1fb65
LamCam <lamcam813@gmail.com>
2024-05-01 05:05:25 +0700

    Create method put quantity for cart

--- backend/src/routes/cart.router.js
+++ backend/src/routes/cart.router.js
@@ -6,7 +6,7 @@ router.get("/", cart.getCart);
 router.post("/getQuantity", cart.getQuantityCartByUserId);
 router.post("/get", cart.getCartByUserId);
 router.get("/delete", cart.deleteAllProductFromCart);
-
+router.put("/update-quantity", cart.putQuantityToCart)
 router.post("/add", cart.addProductToCart);
 router.delete("/delete-item/:id", cart.deleteCartItem);
 
