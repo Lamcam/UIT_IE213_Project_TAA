641b6351ba03ceea18e2003e342a59aa72a614f4
LamCam <lamcam813@gmail.com>
2024-04-30 20:47:47 +0700

    Update hot product in home page

--- frontend/src/components/HomeComponents/HotProducts.jsx
+++ frontend/src/components/HomeComponents/HotProducts.jsx
@@ -1,73 +1,96 @@
-import React, {useState, useEffect} from "react";
-import { Container, Row } from 'react-bootstrap';
-import ProductCard from './ProductCard';
-import Carousel from 'react-bootstrap/Carousel';
-import 'style/components/Home/HotProduct.scss'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-function createCard(product) {
-  
-  const {_id, prod_name, prod_img, prod_cost, prod_discount, prod_num_avai } = product;
-  const cost = prod_cost.$numberDecimal;
-  const discout = prod_discount.$numberDecimal;
-
-    return (
-      <ProductCard
-        key={_id}
-        name={prod_name}
-        imgURL={prod_img}
-        price={cost}
-        discount={discout}
-        status={prod_num_avai > 0 ? "Còn hàng" : "Hết hàng"}
-      />
-    );
-}
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductItem from "components/Products/ProductItem";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import 'style/components/Home/HotProduct.scss'
+import ButtonIcon from "components/Common/ButtonIcon";
 function HotProducts() {
-    const [products, setProducts] = useState([]); 
+  const [products, setProducts] = useState([]);
 
-    const getHotProducts = async () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
       try {
-          const response = await axios.get("http://localhost:8000/products/hot");
-          setProducts(response.data);
-          // console.log(products);
-        } catch (error) {
-          console.error(error);
+        const response = await axios.get("http://localhost:8000/products/hot");
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
       }
-    }
-    
-    useEffect(() => {
-      getHotProducts();
-    },[])
-
-
+    };
+    fetchProducts();
+  }, []);
+  const CustomPrevArrow = (props) => {
+    const { onClick } = props;
     return (
-    <section className="preview_product_section">
-      <Container fluid>
-      <h1 className="title_section"> Sản phẩm nổi bật trong tuần</h1>
-      <Carousel fluid className="carousel_container" >     
-        <Carousel.Item interval={2000}>
-            <Row>
-                {products.slice(0,4).map(createCard)} 
-            </Row>
-        </Carousel.Item>
-
-        <Carousel.Item interval={2000}>
-            <Row>
-                {products.slice(5,9).map(createCard)} 
-            </Row>
-        </Carousel.Item>
-
-        <Carousel.Item interval={2000}>
-            <Row>
-                {products.slice(10,14).map(createCard)} 
-            </Row>
-          
-        </Carousel.Item>
-      </Carousel>
-      </Container>
-    </section>
+      <div className="custom-prev-arrow" onClick={onClick}>
+        <ButtonIcon
+          label={<FaChevronLeft />}
+          labelColor="#785b5b"
+          borderRadius="100%"
+        />
+      </div>
     );
-}
-export default HotProducts;
+  };
 
+  const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div className="custom-next-arrow" onClick={onClick}>
+        <ButtonIcon
+          label={<FaChevronRight />}
+          labelColor="#785b5b"
+          borderRadius="100%"
+        />
+      </div>
+    );
+  };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5, // Số lượng sản phẩm hiển thị trên mỗi slide
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000, // Tốc độ chạy của slider
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
+  return (
+    <section className="preview_product_section">
+      <h1 className="title_section">Sản phẩm nổi bật trong tuần</h1>
+      <Slider {...settings}>
+        {products.map((product) => (
+          <div key={product._id} className="product__list__hot">
+            <ProductItem product={product} />
+          </div>
+        ))}
+      </Slider>
+    </section>
+  );
+}
+
+export default HotProducts;
