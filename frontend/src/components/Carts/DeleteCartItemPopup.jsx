faeae9d48a5c7ccc04065cb2c23b226749c3f538
Xuan Nhi <xuannhi27.work@gmail.com>
2024-05-01 13:32:04 +0700

    Update UI Popup delete cart item

--- frontend/src/components/Carts/DeleteCartItemPopup.jsx
+++ frontend/src/components/Carts/DeleteCartItemPopup.jsx
@@ -27,21 +27,32 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import 'style/components/Carts/DeleteCartItemPopup.scss';
 
 function DeleteCartItemPopup({ showModal, onClose, onConfirmDelete }) {
   return (
-    <Modal show={showModal} onHide={onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Xác nhận xóa</Modal.Title>
+    <Modal show={showModal} onHide={onClose} className="delete-cart-item-popup">
+      <Modal.Header closeButton className="delete__cart__item__header">
+        <Modal.Title className="delete__cart__title">
+          Xác nhận xóa sản phẩm khỏi giỏ hàng
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className="delete__cart__item__body">
         <p>Bạn có chắc chắn muốn xóa?</p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onClose}>
+      <Modal.Footer className="delete__cart__item__footer">
+        <Button
+          variant="secondary"
+          onClick={onClose}
+          className="btn-close-logout btn_clickable_lightcolor_outline"
+        >
           Hủy bỏ
         </Button>
-        <Button variant="primary" onClick={onConfirmDelete}>
+        <Button
+          variant="primary"
+          onClick={onConfirmDelete}
+          className="btn-logout btn_clickable_boldcolor delete__cart__item_btn"
+        >
           Đồng ý
         </Button>
       </Modal.Footer>
