2a3606e77534cfe7bed625bc08d34b1e0a6d9f7b
anhuynh219 <anhuynh200319@gmail.com>
2024-05-07 10:09:29 +0700

    Fix error in order controller

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -16,7 +16,7 @@ const getOrderDetail = async (req, res) => {
     try {
         const orderDetail = await OrderDetail.find({}).populate('prod_id', 'prod_name').populate('order_id', 'order_datetime order_total_cost user_id bank_id pay_id order_is_paying quantity order_status');
         const orderWithUser = await User.populate(orderDetail, { path: 'order_id.user_id', select: 'user_name' });
-        const filtered = orderWithUser.filter((item) => item.prod_id !== null);
+        const filtered = orderWithUser.filter((item) => item.prod_id !== null );
         res.status(200).json(filtered);
     } catch (error) {
         res.status(500).json({ message: error.message });
