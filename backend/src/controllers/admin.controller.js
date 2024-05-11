3eff9f2e125e4e9641200333577380fc94be6751
anhuynh219 <anhuynh200319@gmail.com>
2024-05-11 12:14:36 +0700

    update login for admin

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -16,8 +16,26 @@ const get = async (req, res) => {
   }
 };
 
+const getAdmin = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { username , password} = req.body;
+    const admin = await Admin.findOne({ admin_username: username });
+    if (!admin) {
+      return res.status(404).json({ message: "Không tìm thấy Admin" });
+    }
+    if(password !== admin.admin_password){
+      return res.status(400).json({ message: "Sai mật khẩu"  });
+    }
+    res.status(200).json(admin);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const admin = {
   get,
+  getAdmin,
 };
 
 module.exports = admin;
--- backend/src/routes/admin.router.js
+++ backend/src/routes/admin.router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const admin = require("../controllers/admin.controller");
 
 router.get("/", admin.get);
+router.post("/login", admin.getAdmin);
 
 module.exports = router;
